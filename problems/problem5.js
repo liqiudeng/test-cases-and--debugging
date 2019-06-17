c39fcf059a9b13de2e8da9f8abeff7584a3e9778
liqiu deng <isabelladeng15@gmail.com>
2019-06-17 17:36:28 -0400

    test cases5

--- problems/problem5.js
+++ problems/problem5.js
@@ -1,27 +1,22 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
-// we need 5 test cases. 
-let inputs = [
-  [2, 7]
-]
+// we need 5 test cases.
+let inputs = [[2, 7], [3, 4], [0, 8], [11, 2], [-5, 3]];
 
-let outputs = [
-  14
-]
+let outputs = [14, 12, 0, 22, -15];
 
 /*
 The input of the function is an array.
 Make this function return the product of the two numbers in the array. If the input array length is not 2, or if anything other than numbers are passed, return undefined.
 */
 function f(input) {
-    
+  return input[0] * input[1];
 }
-
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  if (i > inputs.length) throw new Error("You do not have enough test cases");
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -29,4 +24,4 @@ runTest(1);
 runTest(2);
 runTest(3);
 runTest(4);
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
