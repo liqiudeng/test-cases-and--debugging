f418d98df1a64d353749a17e14608dbf5230a6a6
liqiu deng <isabelladeng@liqius-air.wireless.concordia.ca>
2019-06-17 12:20:46 -0400

    debugger

--- problems/problem1.js
+++ problems/problem1.js
@@ -1,21 +1,20 @@
-let verifyEquals = require('./verify-equals.js'); // this line imports the verifyEquals function from the verify-equals.js file
+let verifyEquals = require("./verify-equals.js"); // this line imports the verifyEquals function from the verify-equals.js file
 
 // we need 5 test cases. I provided 1 input
-let inputs = [
-]
+let inputs = ["abc", "cba", "hello", "goodbye", ""];
 
-let outputs = [
-]
+let outputs = ["a", "c", "h", "g", undefined];
 
 // Make this function return the first letter of the string that is passed to it. If the string does not have a first letter, return undefined
 function f(str) {
-    
+  if (str === "") return undefined;
+  return str[0];
 }
 
 function runTest(i) {
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual) // verifyEquals is imported above
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+  verifyEquals(expected, actual); // verifyEquals is imported above
 }
 
 runTest(0);
@@ -23,4 +22,4 @@ runTest(1);
 runTest(2);
 runTest(3);
 runTest(4);
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
