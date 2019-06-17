efff0fc9348fcffd24c33529014d589e67555cb8
liqiu deng <isabelladeng15@gmail.com>
2019-06-17 16:58:38 -0400

    debugger3

--- problems/problem3.js
+++ problems/problem3.js
@@ -1,27 +1,27 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
 // we need 7 test cases. I've provided 2.
-let inputs = [
-  [2, 4],
-  [-3, 3]
-]
+let inputs = [[2, 4], [-3, 3], 3, [2, -5], [0, 8], [9, -3], [23, 34]];
 
-let outputs = [
-  6,
-  0
-]
+let outputs = [6, 0, undefined, -3, 8, 6, 57];
 
 /*
-Make this function return the sum of the two numbers that are passed to it. If the input array length is not 2, or if anything other than numbers are passed, return undefined.
+Make this function return the sum of the two numbers that are passed to it. 
+If the input array length is not 2, or if anything other than numbers are passed, return undefined.
 */
 function f(input) {
-    
+  if (!Array.isArray(input)) return undefined;
+  if (input.length !== 2) return undefined;
+  let sum = 0;
+  for (var i = 0; i < input.length; i++) sum += input[i];
+
+  return sum;
 }
 
 function runTest(i) {
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -31,4 +31,4 @@ runTest(3);
 runTest(4);
 runTest(5);
 runTest(6);
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
