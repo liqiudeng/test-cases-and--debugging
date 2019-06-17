1e813dddd605b1bfaca5fe45837e8b5e28cbce96
liqiu deng <isabelladeng15@gmail.com>
2019-06-17 18:23:09 -0400

    test cases7

--- problems/problem7.js
+++ problems/problem7.js
@@ -1,13 +1,25 @@
-let verifyEquals = require('./verify-equals.js');
+let verifyEquals = require("./verify-equals.js");
 
-// we need 7 test cases. 
+// we need 7 test cases.
 let inputs = [
-  
-]
+  ["go", 2],
+  ["fo", 3],
+  ["too", 4],
+  ["on", 1],
+  ["pop", 2],
+  ["hi", -3],
+  ["so", 0]
+];
 
 let outputs = [
-  
-]
+  "gogo",
+  "fofofo",
+  "tootootootoo",
+  "on",
+  "poppop",
+  undefined,
+  undefined
+];
 
 /*
 The function input is an array as input. The first element of the array is a string. The second is a number.
@@ -21,14 +33,17 @@ f(["fo", 3]) // "fofofo"
 f(["foo", -1]) // ""
 */
 function f(arr) {
-    
+  if (arr[1] <= 0) return undefined;
+  var sum = arr[0];
+  for (var i = 1; i < arr[1]; i++) sum += arr[0];
+  return sum;
 }
 
 function runTest(i) {
-    if(i > inputs.length) throw new Error("You do not have enough test cases");
-    let expected = outputs[i];
-    let actual = f(inputs[i]);
-    verifyEquals(expected, actual)
+  if (i > inputs.length) throw new Error("You do not have enough test cases");
+  let expected = outputs[i];
+  let actual = f(inputs[i]);
+  verifyEquals(expected, actual);
 }
 
 runTest(0);
@@ -38,4 +53,4 @@ runTest(3);
 runTest(4);
 runTest(5);
 runTest(6);
-console.log("All tests passed for " + __filename)
+console.log("All tests passed for " + __filename);
